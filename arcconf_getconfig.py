f49ee67e5832e426b9913097c22695aaa24f67f4
mavriq <admin@mavriq.net>
2017-06-22 20:08:58 +0300

    Сохранено промежуточное состояние разработки

---
+++ arcconf_getconfig.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__version__ = '0.1'
+
+
+import os
+import sys
+import re
+
+
+try:
+    next.__doc__
+except NameError:
+    def next(iterator):
+        '''for python2.4 or earlyer'''
+        return iterator.next()
+
+
+class StackLine(object):
+    def __init__(self):
+        self.result = []
+        self._last_spaces = -1
+        self._stack = [{
+            'spaces': self._last_spaces,
+            'branch': self.result,
+        }]
+        # _stack - стек элементов, в которые добавляются новые записи
+        self._wasdash = False
+
+    @staticmethod
+    def is_multidash(line):
+        return re.compile('^ *-').match(line)
+
+    @staticmethod
+    def lspace_count(line):
+        '''return the number of spaces at begining of the line'''
+        return re.compile('^( *)[^ ].*$').match(line).groups()[0].__len__()
+
+    @staticmethod
+    def get_k_v(line):
+        _m = re.compile('^ *([^ :][^:]+[^ :]) +: +([^ :][^:]+[^ :]) *$')
+        _x = _m.match(line)
+        return _x.groups() if _x else None
+
+    def _crop_stack(self, i, leave_first=False):
+        _new_stack = []
+        for _x in self._stack:
+            if _x['spaces'] < i:
+                _new_stack.append(_x)
+            elif _x['spaces'] == i and leave_first:
+                _new_stack.append(_x)
+                break
+            else:
+                break
+        self._stack = _new_stack
+
+    def _apend_branch(self, spaces, branch):
+        self._stack[-1]['branch'].append(branch)
+        self._stack.append({
+            'spaces': spaces,
+            'branch': branch,
+        })
+
+    def append(self, line):
+        _spaces = self.lspace_count(line)
+        if self.is_multidash(line):
+            self._wasdash = not self._wasdash
+            self._last_spaces = _spaces
+            return
+        if self._wasdash:
+            # начинается заголовок нового списка элементов
+            self._crop_stack(_spaces)
+            self._append_branch(_spaces, [line])
+            self._last_spaces = _spaces
+            return
+        kv = self.get_k_v(line)
+        if kv is None:
+            pass
+
+
+class ArcconfGetconfig(object):
+    u'''
+    Parses the output of the command
+    arcconf getconfig <ID>
+    '''
+
+    def __init__(self, filename=None, id=None):
+        self.id = 1 if id is None else id
+        if filename is None and id is None:
+            if os.isatty(sys.stdin.fileno()):
+                self.id = '1'
+                self.content = self._get_output()
+            else:
+                self.content = sys.stdin.read()
+            pass
+        elif filename:
+            if filename == '-':
+                self.content = sys.stdin.read()
+            else:
+                self.content = open(filename, 'rt').read()
+        else:
+            self.id = id
+            self.content = self._get_output()
+        #
+        self.result = self.parse_config(self.content)
+
+    @staticmethod
+    def _get_output(id):
+        from subprocess import Popen, PIPE
+        try:
+            from subprocess import DEVNULL
+        except ImportError:
+            DEVNULL = open('/dev/null', 'w')
+        _cmdline = 'arcconf getconfig %s' % id
+        _env = os.environ.copy()
+        _env.update(LC_ALL='C', LANG='C', PATH=':'.join((
+            os.environ.get('PATH', ':'),
+            '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin',
+        )))
+        _cmd = Popen(
+            _cmdline,
+            stdin=DEVNULL,
+            stdout=PIPE,
+            stderr=PIPE,
+            shell=True,
+            env=_env,
+        )
+        _x = _cmd.communicate()
+        if _cmd.returncode != 0:
+            raise OSError({
+                'command': _cmdline,
+                'returncode': _cmd.returncode,
+                'stdout': _x[0],
+                'stderr': _x[1],
+            })
+        else:
+            return _x[0].decode()
+
+    @staticmethod
+    def is_multidash(line):
+        return re.compile('^ *-').match(line)
+
+    @staticmethod
+    def lspace_count(line):
+        return re.compile('^( *)[^ ].*$').match(line).groups()[0].__len__()
+
+    @staticmethod
+    def get_k_v(line):
+        _m = re.compile('^ *([^ :][^:]+[^ :]) +: +([^ :][^:]+[^ :]) *$')
+        _x = _m.match(line)
+        return _x.groups() if _x else None
+
+    @staticmethod
+    def parse_config(self, content):
+        _result = []
+        for line in content.splitlines():
+            _result.append()
+        pass
+
+
+def main(*argv):
+    import optparse
+    parser = optparse.OptionParser(usage='%prog: [id]|--input=FILE')
+    # parser.add_option('id')
+    parser.add_option(
+        '-i', '--input',
+        dest='filename',
+        metavar='FILE',
+        help='Read arcconf message from file',
+    )
+    (options, args) = parser.parse_args(argv)
+    _result = []
+    if options.filenane is not None:
+        if args:
+            parser.error('Mutially expusive options <id> and <--input=FILE>')
+        else:
+            _result.append(ArcconfGetconfig(filename=options.filename))
+    elif not args:
+        _result.append(ArcconfGetconfig())
+    else:
+        for i in args:
+            _result.append(ArcconfGetconfig(id=i))
+
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
